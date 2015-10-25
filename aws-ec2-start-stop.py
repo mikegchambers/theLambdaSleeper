eecf5cad2f2fcf739346af8922c2bede18d05b60
mikegchambers <mike@mikegchambers.com>
2015-10-25 22:19:35 +1000

    ELB Support and better names for vars and funcs.
    Added support for ELB.  Now, if an instance is started that is
    registered with an ELB, it will automatically be re-registered to ensure
    its connected.

--- aws-ec2-start-stop.py
+++ aws-ec2-start-stop.py
@@ -1,109 +1,207 @@
 #!/usr/bin/env python
 
 import boto3
-
 from croniter import croniter
 from datetime import datetime
 
 class thelambdasleeper:
 
-	awsregionname='ap-southeast-2' # REQUIRED, string, the AWS region that contains the target instances. 
-	awsvpclist=["vpc-xxxxxxx"]    # REQUIRED, string list, the ids of the VPC(s) that contain the target instances.
-	awsinstanceidlist=["i-xxxxxxx", "i-xxxxxxx", "i-xxxxxxx"] #OPTIONAL, string list (or None), a white-list of instance ids to target.
-	snstopicarn='arn:aws:sns:ap-southeast-2:xxxxxxxxxxxxxx:theLambdaSleeper' #OPTINAL, string, the ARN of a SNS topic to message.
+	# Setting vars:
 
-	ec2client = ""
-	response = ""
+	# name: awsRegionName 
+	# REQUIRED
+	# type: string
+	# Comment: The AWS region that contains the target instances. 
+	awsRegionName='ap-southeast-2' 
+	
+	# name: awsVpcList 
+	# REQUIRED
+	# type: string list
+	# Comment: The ids of the VPC(s) that contain the target instances. 
+	awsVpcList=["vpc-xxxxxxx"] 
+	
+	# name: awsInstanceIdList 
+	# OPTIONAL
+	# type: string list
+	# Comment: A white-list of instance ids to target. 
+	#awsInstanceIdList=["i-xxxxxxx", "i-xxxxxxx", "i-xxxxxxx"] 
+	
+	# name: snsTopicArn 
+	# OPTIONAL
+	# type: string
+	# Comment: The ARN of a SNS topic to message.
+	#snsTopicArn='arn:aws:sns:ap-southeast-2:xxxxxxxxxxxxxx:theLambdaSleeper'
+
+	# Vars used by the class:
+
+	ec2Client = ""
+	elbClient = ""
+	elbMembers = {}	
+	describe_instances = ""
 	message = ""
 
 	def __init__(self):
 		print ('INFO: theLambdaSleeper has started')
-		self.loadec2client()
-		self.loadinstancedata()
-		self.iterateinstances()
+		
+		# Load the AWS clients
+		self.loadEc2Client()
+		self.loadElbClient()
+
+		# Get details of the instances
+		self.loadInstanceData()
+		self.loadInstanceElbMembership()
+		
+		# Main loop through instances
+		self.iterateInstances()
+
+		# Send SNS notification
 		print ('INFO: The message: {}'.format(self.message))
-		self.sendnotification()
+		self.sendNotification()
+		
 		print ('INFO: theLambdaSleeper has completed')
 						
-	def loadec2client(self):
+	def loadEc2Client(self):
 		try:
-			self.ec2client = boto3.client('ec2',  region_name=self.awsregionname)
+			self.ec2Client = boto3.client('ec2',  region_name=self.awsRegionName)
 		except:
-			exit("ERROR: Failed to connect to EC2")
+			exit("ERROR: Failed to connect to EC2 (boto3 ec2 client)")
 		
-	def loadinstancedata(self):
+	def loadElbClient(self):
+		try:
+			self.elbClient = boto3.client('elb',  region_name=self.awsRegionName)
+		except:
+			exit("ERROR: Failed to connect to ELB (boto3 elb client)")
+		
+	def loadInstanceElbMembership(self):
+		try:
+			describe_load_balancers = self.elbClient.describe_load_balancers()
+			for elb in describe_load_balancers['LoadBalancerDescriptions']:
+				for i in elb['Instances']:
+					self.elbMembers[ i['InstanceId'] ] = elb['LoadBalancerName']
+		except:
+			print('ERROR: Failed to get ELB membership details. (describe_load_balancers)' )
+		
+	def loadInstanceData(self):
 		try:
 			try:
-				self.awsinstanceidlist
+				self.awsInstanceIdList
 			except:
-				self.response = self.ec2client.describe_instances(Filters=[{"Name": "vpc-id", "Values": self.awsvpclist}])
+				self.describe_instances = self.ec2Client.describe_instances(Filters=[{"Name": "vpc-id", "Values": self.awsVpcList}])
 			else:
-				self.response = self.ec2client.describe_instances(Filters=[{"Name": "vpc-id", "Values": self.awsvpclist}, {"Name": "instance-id", "Values": self.awsinstanceidlist}])
+				self.describe_instances = self.ec2Client.describe_instances(Filters=[{"Name": "vpc-id", "Values": self.awsVpcList}, {"Name": "instance-id", "Values": self.awsInstanceIdList}])
 		except:
 			exit("ERROR: Failed to get EC2 instance data. (describe_instances)")
 		
-	def iterateinstances(self):
-		for reservation in self.response['Reservations']:
+	def iterateInstances(self):
+
+		for reservation in self.describe_instances['Reservations']:
 			for instance in reservation['Instances']:
-			
-				instancedetails = self.getdetailsforinstance( instance )
-				print ( 'INFO: Found instance details: {}'.format(instancedetails))
+
+				instanceDetails = self.getDetailsForInstance( instance )
+				print ( 'INFO: Found instance details: {}'.format(instanceDetails))
 				
-				if instancedetails['timestartstop']:
+				if instanceDetails['timestartstop']:
 					
-					targetstatus = self.gettargetstatusfromtag(instancedetails['timestartstop'])
+					targetState = self.gettargetStateFromTag(instanceDetails['timestartstop'])
 					
-					if targetstatus:
-						print ( 'INFO: Target status of instance {} is {}'.format(instancedetails['id'], targetstatus))
+					if targetState:
+						print ( 'INFO: Target state of instance {} is {}'.format(instanceDetails['id'], targetState))
 						
-						if targetstatus != instancedetails['status']:
-							if instancedetails['status'] != 'running':
-								self.message = self.message + "Starting instance ({}) {} \n".format(instancedetails['id'], instancedetails['name'])
-								self.startinstance( instancedetails['id'] )
+						if targetState != instanceDetails['state']:
+							# The instance is not in the state that it should be:
+
+							if instanceDetails['state'] != 'running':
+								# The instance should be running:								
+								self.message = self.message + "Starting instance ({}) {} \n".format(instanceDetails['id'], instanceDetails['name'])
+								self.startInstance( instanceDetails['id'] )
+								
+								try:
+									# If the instance is a member of an ELB, re-register it with that ELB.
+									
+									# Why?   :
+
+									# Problem:  The instances launched in a VPC have been stopped and then started. 
+									#           The load balancer is not able to connect to the restarted instance.
+									# Cause:    When you stop and then start your VPC instance, it might take some 
+									#		    time for the load balancer to recognize that the instance has restarted. 
+									#           During this time, the load balancer is not connected with the restarted instance.
+									# Solution: Re-register the instance with the load balancer after the restart. For 
+									#           more information, see De-register and Register EC2 Instances with Your 
+									#           Load Balancer.
+									# Source:   http://docs.aws.amazon.com/ElasticLoadBalancing/latest/DeveloperGuide/ts-elb-healthcheck.html
+
+									if self.elbMembers[instanceDetails['id']]:
+										print ('INFO: This instance ({}) is a member of the elb: {}'.format( instanceDetails['id'], self.elbMembers[instanceDetails['id']] ))
+										self.message = self.message + "Instance {} ({}) is registered with ELB {} and has been reregistered \n".format(instanceDetails['id'], instanceDetails['name'], self.elbMembers[instanceDetails['id']])
+										self.reRegisterInstanceWithElb( self.elbMembers[instanceDetails['id']], instanceDetails['id'] )
+								except:
+									pass
 							else:
-								self.message = self.message + "Stopping instance ({}) {} \n".format(instancedetails['id'], instancedetails['name'])
-								self.stopinstance( instancedetails['id'] )
+								# The instance should be stopped:
+								self.message = self.message + "Stopping instance ({}) {} \n".format(instanceDetails['id'], instanceDetails['name'])
+								self.stopInstance( instanceDetails['id'] )
 					else:
-						print ( 'WARNING: Unable to READ timestartstop tag for instance {}'.format(instancedetails['id']))
+						print ( 'WARNING: Unable to READ timestartstop tag for instance {}'.format(instanceDetails['id']))
 				else:
-					print ( 'WARNING: Unable to FIND timestartstop tag for instance {}'.format(instancedetails['id']) )
+					print ( 'WARNING: Unable to FIND timestartstop tag for instance {}'.format(instanceDetails['id']) )
 	
-	def getdetailsforinstance(self, instancedata):
+	def getDetailsForInstance(self, instancedata):
 		try:
-			instanceid = instancedata['InstanceId']
-			instancestatename = instancedata['State']['Name']
+			instanceId = instancedata['InstanceId']
+			instanceStateName = instancedata['State']['Name']
 		except:
 			print ('ERROR: An unexpected error occured.  Failed to get basic instace details.')
 			return
 
-		instagestartstoptag = self.getvaluefortag( 'timestartstop', instancedata )
-		instancename = self.getvaluefortag( 'Name', instancedata )
+		instanceStartStopTag = self.getValueForTag( 'timestartstop', instancedata )
+		instanceName = self.getValueForTag( 'Name', instancedata )
 
-		return {'id':instanceid, 'status':instancestatename, 'timestartstop':instagestartstoptag, 'name':instancename}
+		return {'id':instanceId, 'state':instanceStateName, 'timestartstop':instanceStartStopTag, 'name':instanceName}
 		
-	def getvaluefortag(self, tag, instancedata):
+	def getValueForTag(self, tag, instancedata):
 		try:
 			for tags in instancedata['Tags']:
 				if tags['Key'] == tag:
 					return tags['Value']
+			return False
 		except:
-			print ('An unexpected error occured when trying to get the "{}" tag.'.format(tag))
+			print ('ERROR: An unexpected error occured when trying to get the "{}" tag.'.format(tag))
 			return False
 	
 	
-	def startinstance(self, instanceid):
+	def startInstance(self, instanceid):
 		print('INFO: Request to start {}'.format(instanceid))
-		response = self.ec2client.start_instances(InstanceIds=[instanceid])
-		print(response)
-		
+		try:
+			start_instances = self.ec2Client.start_instances(InstanceIds=[instanceid])
+			print(start_instances)
+		except:
+			print('ERROR: There was an error trying to start instance {}'.format(instanceid) )
 	
-	def stopinstance(self, instanceid):
+	def stopInstance(self, instanceid):
 		print('INFO: Request to stop {}'.format(instanceid))
-		response = self.ec2client.stop_instances(InstanceIds=[instanceid])
-		print(response)
-		
-		
-	def gettargetstatusfromtag(self, startstoptag):
+		try:
+			stop_instances = self.ec2Client.stop_instances(InstanceIds=[instanceid])
+			print(stop_instances)
+		except:
+			print('ERROR: There was an error trying to stop instance {}'.format(instanceid) )
+
+	def reRegisterInstanceWithElb(self, elbname, instanceid):
+		print('INFO: DEregistering instance from ELB')
+		try:
+			deregister_instances_from_load_balancer = self.elbClient.deregister_instances_from_load_balancer( LoadBalancerName=elbname,Instances=[{'InstanceId': instanceid}])
+			print(deregister_instances_from_load_balancer)
+		except:
+			print('ERROR: There was an error trying to deregister instance {} from elb {}'.format(instanceid, elbname) )
+
+		print('INFO: Registering instance from ELB')
+		try:
+			register_instances_with_load_balancer = self.elbClient.register_instances_with_load_balancer( LoadBalancerName=elbname,Instances=[{'InstanceId': instanceid}])
+			print(register_instances_with_load_balancer)
+		except:
+			print('ERROR: There was an error trying to register instance {} from elb {}'.format(instanceid, elbname) )
+
+
+	def gettargetStateFromTag(self, startstoptag):
 		
 		try:
 			startStopTimesList = startstoptag.strip('"').split("|")
@@ -126,13 +224,17 @@ class thelambdasleeper:
 		else:
 			return 'stopped'
 
-	def sendnotification(self):
-		if self.message != '' and self.snstopicarn:
-			try:
-				snsclient = boto3.client('sns',  region_name=self.awsregionname)
-				snsclient.publish( TopicArn=self.snstopicarn, Message=self.message, Subject='theLambdaSleeper - stop start notification')
-			except:
-				print('WARNING: Failed to publish SNS notification.')
+	def sendNotification(self):
+		try:
+			if self.message != '' and self.snsTopicArn:
+				try:
+					snsclient = boto3.client('sns',  region_name=self.awsRegionName)
+					publish = snsclient.publish( TopicArn=self.snsTopicArn, Message=self.message, Subject='theLambdaSleeper - stop start notification')
+					print ( publish )
+				except:
+					print('WARNING: Failed to publish SNS notification.')
+		except:
+			pass
 			
 def lambda_handler(event, context):
 	tls = thelambdasleeper()
